a7dea5ceca932c58211834539046fbd835bd2514
Qayyuum <aayqayoom@gmail.com>
2019-02-10 20:56:48 +0800

    Java Tutorial 2

---
+++ Java/T2/src/T2/Speed.java
@@ -0,0 +1,33 @@
+
+
+//Write a program that will compute and display the 
+// average speed of a vehicle. 
+// Declares two floating-point variables named distance 
+// travelled (in km) and time taken (in hour) respectively.
+//Both variables’ values are to be entered through 
+// keyboard. Save the program as Speed.java.
+
+import java.util.Scanner;
+
+public class Speed {
+    
+    public static void main(String[] args){    
+        float distance;
+        float time;
+        float speed;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter distance: " );
+        distance = input.nextFloat();
+        
+        System.out.print("Enter time: " );
+        time = input.nextFloat();
+        
+        speed = distance / time;
+        System.out.print("Average speed is : " + speed + " KM/Hour");
+        
+                
+                
+    }
+}
---
+++ Java/T2/src/T2/TimeConversion.java
@@ -0,0 +1,27 @@
+// Write a program which assigns an integer value that 
+// represents a time in second to a variable of 
+// type ‘int’, calculates and prints out this time 
+// in x hours, y minutes, and z seconds.
+// The followings are some sample runs of this program:
+// 600 seconds = 0 hours 10 minutes 0 seconds
+// 3601 seconds = 1 hours 0 minutes 1 seconds
+// 7200 seconds = 2 hours 0 minutes 0 seconds
+// Hint: Use only operator such as % and /
+public class TimeConversion {
+    
+    public static void main(String[] args){
+    
+    int time = 7270;
+    int res_hours, res_min, res_sec;
+    res_hours = time / 3600;
+    time = time % 3600;
+    res_min = time / 60;
+    time = time % 60;
+    res_sec = time;
+    
+    System.out.print( time + " seconds = " + res_hours + " hours " 
+    + res_min + " minutes " + res_sec + " seconds ");
+       
+    }
+    
+}
