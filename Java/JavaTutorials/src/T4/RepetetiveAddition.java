33a19d3d09f953a9fef1ae171047b84582741c9a
Qayyuum <aayqayoom@gmail.com>
2019-02-19 10:11:41 +0800

    T4 - iterations

--- Java/JavaTutorials/src/T4/RepetetiveAddition.java
+++ Java/JavaTutorials/src/T4/RepetetiveAddition.java
@@ -3,30 +3,38 @@ package T4;
 
 import java.util.Scanner;
 
-public class RandomAddition {
+
+public class RepetetiveAddition {
     
-    public static void main(String[] args){
+    public static void main( String[] args) {
         
-        int num1, num2;
+        int num1 , num2;
+        
+        int count = 0;
+        int  MAX = 10;
         
         Scanner kbd = new Scanner(System.in);
         
-        num1 =(int) ( Math.random() * 10 );
-        //System.out.println(" Num1 : " + num1);
-        num2 = (int) ( Math.random() * 10 );
-        //System.out.println(" Num1 : " + num2);
         
-        System.out.print(" What is  " + num1 + "  + "
+        while (count <= MAX) {
+              num1 = (int) ( Math.random() * 10 );
+              num2 = (int) ( Math.random() * 10 );
+               System.out.print(" What is  " + num1 + "  + "
         + num2 + " ?  " );
+               
         int userAnswer = kbd.nextInt();
         
         if ( userAnswer == ( num1 + num2))
             System.out.println(" Your answer is correct!");
         else
             System.out.println( " Oops The Answer is " + (num1 + num2));
-       
+        
+        count++;             
+              
+        }
         
         
+      
         
         
         
