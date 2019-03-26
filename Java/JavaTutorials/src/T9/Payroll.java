4df6f626aaf603a304fb153ced5fcf0a88486490
Qayyuum <aayqayoom@gmail.com>
2019-03-26 09:43:58 +0800

    arrays

---
+++ Java/JavaTutorials/src/T9/Payroll.java
@@ -0,0 +1,35 @@
+
+package T9;
+
+import java.util.Scanner;
+import jdk.management.resource.internal.TotalResourceContext;
+
+
+public class Payroll {
+    
+    public static void main( String[] args) {
+        
+        int SIZE = 10;
+        
+        double[] salary;
+        double totalSalary = 0;
+        salary = new double[SIZE];
+        Scanner input = new Scanner( System.in);
+        
+        for ( int i = 0 ; i < SIZE ; i++) {            
+            System.out.println( " Enter salary of Emp-" + (i+1) );
+            salary[i] = input.nextDouble();   
+            totalSalary = totalSalary + salary[i];
+        }
+        
+        System.out.println( " Total Salary : " + totalSalary);
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+}
